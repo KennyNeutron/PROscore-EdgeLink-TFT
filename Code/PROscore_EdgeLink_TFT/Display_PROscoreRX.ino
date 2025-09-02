f2af6112477e1a09de9106e0ca61602a41c9f439
KennyNeutron <kennyneutron.hex@gmail.com>
2025-09-03 01:38:58 +0800

    added Fouls and Time Outs in PROscoreRX

--- Code/PROscore_EdgeLink_TFT/Display_PROscoreRX.ino
+++ Code/PROscore_EdgeLink_TFT/Display_PROscoreRX.ino
@@ -38,8 +38,10 @@ void Display_PROscoreRX() {
     Period = 3;
     ShotClock_Second = 24; ShotClock_Millis = 0;
     HomeScore = 20; GuestScore = 20;
+    HomeFoul = 4; GuestFoul = 3;
+    HomeTOut = 2; GuestTOut = 3;
 
-    // Game time section
+    // Game time Section
     lv_obj_t* Label_GameTime_Text = create_label(scr, "GAME TIME", &lv_font_montserrat_18, lv_color_white());
     lv_obj_align(Label_GameTime_Text, LV_ALIGN_TOP_MID, 5, 5);
 
@@ -48,16 +50,8 @@ void Display_PROscoreRX() {
     lv_obj_t* Label_GameTime = create_label(scr, time_str, &lv_font_montserrat_48, lv_color_hex(0xFF8C00));
     lv_obj_align(Label_GameTime, LV_ALIGN_TOP_MID, 0, 20);
 
-    // Period section
-    lv_obj_t* Label_Period_Text = create_label(scr, "QTR", &lv_font_montserrat_14, lv_color_white());
-    lv_obj_align(Label_Period_Text, LV_ALIGN_BOTTOM_MID, 0, -40);
 
-    char STR_Period[3];
-    snprintf(STR_Period, sizeof(STR_Period), "%d", Period);
-    lv_obj_t* Label_Period = create_label(scr, STR_Period, &lv_font_montserrat_40, lv_color_hex(0x00FFFF));
-    lv_obj_align(Label_Period, LV_ALIGN_BOTTOM_MID, 0, 0);
-
-    // Shot clock section
+    // ShotClock Section
     lv_obj_t* Label_ShotClock_Text = create_label(scr, "SHOT CLOCK", &lv_font_montserrat_18, lv_color_white());
     lv_obj_align(Label_ShotClock_Text, LV_ALIGN_CENTER, 0, -30);
 
@@ -67,7 +61,7 @@ void Display_PROscoreRX() {
     lv_obj_align(Label_ShotClock, LV_ALIGN_CENTER, 0, 0);
     lv_obj_set_style_text_align(Label_ShotClock, LV_TEXT_ALIGN_CENTER, 0);
 
-    // Scores section
+    // Scores Section
     // Home team
     lv_obj_t* Label_HomeTeam_Text = create_label(scr, "HOME", &lv_font_montserrat_18, lv_color_white());
     lv_obj_align(Label_HomeTeam_Text, LV_ALIGN_BOTTOM_LEFT, 21, -44);
@@ -87,6 +81,52 @@ void Display_PROscoreRX() {
     lv_obj_t* Label_GuestScore = create_label(scr, STR_GuestScore, &lv_font_montserrat_48, lv_color_hex(0xFF0000));
     lv_obj_align(Label_GuestScore, LV_ALIGN_BOTTOM_RIGHT, -12, 0);
 
+    //Foul Section
+    //Home Foul
+    lv_obj_t* Label_HomeFoul_Text = create_label(scr, "FOUL", &lv_font_montserrat_14, lv_color_white());
+    lv_obj_align(Label_HomeFoul_Text, LV_ALIGN_LEFT_MID, 12, -0);
+
+    char STR_HomeFoul[3];
+    snprintf(STR_HomeFoul, sizeof(STR_HomeFoul), "%d", HomeFoul);
+    lv_obj_t* Label_HomeFoul = create_label(scr, STR_HomeFoul, &lv_font_montserrat_36, lv_color_hex(0xFF00FF));
+    lv_obj_align(Label_HomeFoul, LV_ALIGN_LEFT_MID, 18, 20);
+
+    //Guest Foul
+    lv_obj_t* Label_GuestFoul_Text = create_label(scr, "FOUL", &lv_font_montserrat_14, lv_color_white());
+    lv_obj_align(Label_GuestFoul_Text, LV_ALIGN_RIGHT_MID, -12, 0);
+
+    char STR_GuestFoul[3];
+    snprintf(STR_GuestFoul, sizeof(STR_GuestFoul), "%d", GuestFoul);
+    lv_obj_t* Label_GuestFoul = create_label(scr, STR_GuestFoul, &lv_font_montserrat_36, lv_color_hex(0xFF00FF));
+    lv_obj_align(Label_GuestFoul, LV_ALIGN_RIGHT_MID, -18, 20);
+
+    //Time Out Section
+    //Home Time Out
+    lv_obj_t* Label_TimeOut_Text = create_label(scr, "T.OUT", &lv_font_montserrat_14, lv_color_white());
+    lv_obj_align(Label_TimeOut_Text, LV_ALIGN_LEFT_MID, 12, -60);
+
+    char STR_TimeOut[3];
+    snprintf(STR_TimeOut, sizeof(STR_TimeOut), "%d", HomeTOut);
+    lv_obj_t* Label_TimeOut = create_label(scr, STR_TimeOut, &lv_font_montserrat_36, lv_color_hex(0xFFFF00));
+    lv_obj_align(Label_TimeOut, LV_ALIGN_LEFT_MID, 18, -40);
+
+    //Guest Time Out
+    lv_obj_t* Label_GuestTimeOut_Text = create_label(scr, "T.OUT", &lv_font_montserrat_14, lv_color_white());
+    lv_obj_align(Label_GuestTimeOut_Text, LV_ALIGN_RIGHT_MID, -12, -60);
+
+    char STR_GuestTimeOut[3];
+    snprintf(STR_GuestTimeOut, sizeof(STR_GuestTimeOut), "%d", GuestTOut);
+    lv_obj_t* Label_GuestTimeOut = create_label(scr, STR_GuestTimeOut, &lv_font_montserrat_36, lv_color_hex(0xFFFF00));
+    lv_obj_align(Label_GuestTimeOut, LV_ALIGN_RIGHT_MID, -18, -40);
+
+    // Period Section
+    lv_obj_t* Label_Period_Text = create_label(scr, "QTR", &lv_font_montserrat_14, lv_color_white());
+    lv_obj_align(Label_Period_Text, LV_ALIGN_BOTTOM_MID, 0, -40);
+
+    char STR_Period[3];
+    snprintf(STR_Period, sizeof(STR_Period), "%d", Period);
+    lv_obj_t* Label_Period = create_label(scr, STR_Period, &lv_font_montserrat_36, lv_color_hex(0x00FFFF));
+    lv_obj_align(Label_Period, LV_ALIGN_BOTTOM_MID, 0, 0);
     Display_PROscoreRX_Init = true;
   }
 }
